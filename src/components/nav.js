93bd7ba430672c3320173053fa091606ad6535e5
haidedang <haiduc.dang91@gmail.com>
2020-11-15 20:44:13 +0100

    username changed

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, StaticQuery, graphql } from "gatsby";
+import { USERNAME } from "../config/config";
 
 const Nav = () => (
   <StaticQuery
@@ -20,28 +21,56 @@ const Nav = () => (
     `}
     render={(data) => (
       <div>
-     <section >
-        <div >
+        <section>
+          <div>
             <nav className="navbar navbar-light navbar-expand-md">
-                <div className="container-fluid"><a className="navbar-brand" href="/">CD</a><button data-toggle="collapse" className="navbar-toggler" data-target="#navcol-2"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
-                    <div className="collapse navbar-collapse"
-                        id="navcol-2">
-                        <ul className="nav navbar-nav mx-auto">
-                            <li className="nav-item" role="presentation"><a className="nav-link active" href="/">Blog</a></li>
-                            <li className="nav-item" role="presentation"><a className="nav-link" href="/shoutout">Shoutouts</a></li>
-                           {/*  <li className="nav-item" role="presentation"><a className="nav-link" href="#">Shop</a></li> */}
-                            <li className="nav-item" role="presentation"></li>
-                        </ul>
-                        <ul className="nav navbar-nav">
-                            <li className="nav-item" role="presentation"></li>
-                            <li className="nav-item" role="presentation"><a className="nav-link" href="#"><i style={{"font-size": "30px"}} className="fa fa-instagram"></i></a></li>
-                        </ul>
-                    </div>
+              <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                  CD
+                </a>
+                <button
+                  data-toggle="collapse"
+                  className="navbar-toggler"
+                  data-target="#navcol-2"
+                >
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navcol-2">
+                  <ul className="nav navbar-nav mx-auto">
+                    <li className="nav-item" role="presentation">
+                      <a className="nav-link active" href="/">
+                        Blog
+                      </a>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                      <a className="nav-link" href="/shoutout">
+                        Shoutouts
+                      </a>
+                    </li>
+                    {/*  <li className="nav-item" role="presentation"><a className="nav-link" href="#">Shop</a></li> */}
+                    <li className="nav-item" role="presentation"></li>
+                  </ul>
+                  <ul className="nav navbar-nav">
+                    <li className="nav-item" role="presentation"></li>
+                    <li className="nav-item" role="presentation">
+                      <a
+                        className="nav-link"
+                        href={`https://instagram.com/${USERNAME}`}
+                      >
+                        <i
+                          style={{ "font-size": "30px" }}
+                          className="fa fa-instagram"
+                        ></i>
+                      </a>
+                    </li>
+                  </ul>
                 </div>
+              </div>
             </nav>
-        </div>
-    </section>
-  </div>
+          </div>
+        </section>
+      </div>
 
       /* <div>
         <div>
--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,6 @@
-export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://cottage-core.herokuapp.com'
-  : 'http://localhost:8080'
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://cottage-core.herokuapp.com"
+    : "http://localhost:8080";
+
+export const USERNAME = "cottagecoredream_";
