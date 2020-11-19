00040f6fb15b7be021381225fd5924a712363ff6
haidedang <haiduc.dang91@gmail.com>
2020-11-19 11:28:07 +0100

    change to comments.cottagecoredream.com

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,7 @@ module.exports = {
     {
       resolve: "@debiki/gatsby-plugin-talkyard",
       options: {
-        talkyardServerUrl:
-          "https://comments-for-cottagecoredream-com.talkyard.net",
+        talkyardServerUrl: "https://comments.cottagecoredream.com",
       },
     },
     {
