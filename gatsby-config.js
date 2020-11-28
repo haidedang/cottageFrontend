cc7df6b0461446724cc52b40711b39d819e09e29
haidedang <haiduc.dang91@gmail.com>
2020-11-28 17:17:41 +0100

    update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,15 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-7KM0NT19S7", // Google Analytics / GA
+        ],
+      },
+    },
+    {
       resolve: "@debiki/gatsby-plugin-talkyard",
       options: {
         talkyardServerUrl: "https://comments.cottagecoredream.com",
